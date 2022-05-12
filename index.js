615bef4b090c69c03fcb0febe100abcd78b26b18
Alan Costa <alanc15costa@hotmail.com>
2022-05-11 21:50:00 -0300

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const petRouter = require('./routes/pets.js')
+
+app.use(petRouter); /* Usar a importação do petRouter */
+
+//Criar rota de servicos
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 })
 
+
 app.listen(3000, () => console.log('Rodando....'))
+
+
+
